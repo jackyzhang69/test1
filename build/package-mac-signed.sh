670a587bc7ac291cfdb4f51600739cc5a825ea6b
Yansong Li <yansongw10@outlook.com>
2025-02-16 16:20:04 +0800

    Add mac build signing

---
+++ build/package-mac-signed.sh
@@ -0,0 +1,6 @@
+export APPLE_ID="losnoth@hotmail.com" 
+export APPLE_APP_SPECIFIC_PASSWORD="jzhx-qzvl-heex-dpnp" 
+export APPLE_TEAM_ID="K7GGZ8W679" 
+export CSC_LINK="~/certificate.p12" 
+export CSC_KEY_PASSWORD="1234Poiu?" 
+npm run package
